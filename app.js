7ac88fca3480cf6ed73c1ccf3bed6eeb9c7f70ab
Shivam Mishra <mshiv619@gmail.com>
2021-06-10 00:51:30 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output")
 
-console.log(txtInput)
 
 function clickHandler() {
-    console.log("clicked");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "xyz  " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)

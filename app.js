f47a70dff199e2a4ac672ec353d35356261efbf7
Shivam Mishra <mshiv619@gmail.com>
2021-06-10 01:18:52 +0530

    updated minion server URL
    updated minion URL and fetched it

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output")
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
 
 function clickHandler() {
-    outputDiv.innerText = "xyz  " + txtInput.value;
+    // outputDiv.innerText = "xyz  " + txtInput.value;
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response=> response.json())
+    .then(json=> console.log(json.contents.translation))
+
+
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-

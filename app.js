5bcf141ba8fad159b1f4e4c0638048f088c31369
Shivam Mishra <mshiv619@gmail.com>
2021-06-09 23:34:46 +0530

    taking input and console
    when something is written in textarea and button is clicked we got same in console

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
-console.log(btnTranslate)
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput)
+
+function clickHandler() {
+    console.log("clicked");
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+

d511a707062bc54bc083b0ed0b54376fe6723a2b
Shivam Mishra <mshiv619@gmail.com>
2021-06-10 01:52:07 +0530

    updated error handler
    lastly added error handler to complete this

--- app.js
+++ app.js
@@ -2,22 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output")
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
 
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text
 }
 
-function clickHandler() {
-    // outputDiv.innerText = "xyz  " + txtInput.value;
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
+function clickHandler() {
     var inputText = txtInput.value;
 
+    // calling server
     fetch(getTranslationURL(inputText))
-    .then(response=> response.json())
-    .then(json=> console.log(json.contents.translation))
-
-
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
